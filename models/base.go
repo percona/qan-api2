4976f000b10acfdffa0e7ea8108fa5bebbf34e44
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2019-05-22 18:02:23 +0300

    PMM-4004 Remove dead code.

--- models/base.go
+++ models/base.go
@@ -17,55 +17,10 @@
 package models
 
 import (
-	"fmt"
-	"math"
-	"reflect"
-	"strconv"
-
 	"github.com/percona/pmm/api/qanpb"
 )
 
-func getFloat(unk interface{}) (float64, error) {
-	floatType := reflect.TypeOf(float64(0))
-	stringType := reflect.TypeOf("")
-
-	switch i := unk.(type) {
-	case float64:
-		return i, nil
-	case float32:
-		return float64(i), nil
-	case int64:
-		return float64(i), nil
-	case int32:
-		return float64(i), nil
-	case int:
-		return float64(i), nil
-	case uint64:
-		return float64(i), nil
-	case uint32:
-		return float64(i), nil
-	case uint:
-		return float64(i), nil
-	case string:
-		return strconv.ParseFloat(i, 64)
-	default:
-		v := reflect.ValueOf(unk)
-		v = reflect.Indirect(v)
-		switch {
-		case v.Type().ConvertibleTo(floatType):
-			fv := v.Convert(floatType)
-			return fv.Float(), nil
-		case v.Type().ConvertibleTo(stringType):
-			sv := v.Convert(stringType)
-			s := sv.String()
-			return strconv.ParseFloat(s, 64)
-		default:
-			return math.NaN(), fmt.Errorf("can't convert %v to float64", v.Type())
-		}
-	}
-}
-
-// order matter.
+//nolint
 var sparklinePointAllFields = []string{
 	"point",
 	"timestamp",
