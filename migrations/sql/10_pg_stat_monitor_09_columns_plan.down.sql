11ae38d4c75e8fa065cc60d7ff1ec3fa7aad586e
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-06-11 10:04:12 +0200

    PMM-8227 Remove field.

--- migrations/sql/10_pg_stat_monitor_09_columns_plan.down.sql
+++ migrations/sql/10_pg_stat_monitor_09_columns_plan.down.sql
@@ -2,5 +2,4 @@ ALTER TABLE metrics
   DROP COLUMN `m_wal_bytes`,
   DROP COLUMN `m_plan_total_time`,
   DROP COLUMN `m_plan_min_time`,
-  DROP COLUMN `m_plan_max_time`,
-  DROP COLUMN `m_plan_mean_time`;
+  DROP COLUMN `m_plan_max_time`;
