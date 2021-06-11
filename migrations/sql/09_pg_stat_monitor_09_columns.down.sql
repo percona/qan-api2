8c3df66bb5e505626a6f2c2aaed89db575966936
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-06-11 09:40:35 +0200

    PMM-8116 Fix typo.

--- migrations/sql/09_pg_stat_monitor_09_columns.down.sql
+++ migrations/sql/09_pg_stat_monitor_09_columns.down.sql
@@ -1,6 +1,6 @@
 ALTER TABLE metrics
-  DROP COLUMN `m_plans_calls__cnt`,
-  DROP COLUMN `m_plans_calls__sum`,
+  DROP COLUMN `m_plans_calls_cnt`,
+  DROP COLUMN `m_plans_calls_sum`,
   DROP COLUMN `m_wal_records_cnt`,
   DROP COLUMN `m_wal_records_sum`,
   DROP COLUMN `m_wal_fpi_cnt`,
