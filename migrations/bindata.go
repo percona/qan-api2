211c8da0a5bda315dba6313fdb1e371eb8d65e7a
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2022-10-18 17:11:08 +0200

    PMM-10385 Format.

--- migrations/bindata.go
+++ migrations/bindata.go
@@ -829,11 +829,11 @@ const AssetDebug = false
 // For example if you run go-bindata on data/... and data contains the
 // following hierarchy:
 //
-//	data/
-//	  foo.txt
-//	  img/
-//	    a.png
-//	    b.png
+//     data/
+//       foo.txt
+//       img/
+//         a.png
+//         b.png
 //
 // then AssetDir("data") would return []string{"foo.txt", "img"},
 // AssetDir("data/img") would return []string{"a.png", "b.png"},
